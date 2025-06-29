83a6b12c85ba78b74351fa7046070073436fbb1d
Marwan Jaml <marwanaljaml11@gmail.com>
2025-06-29 21:32:20 +0300

    Last-Deploy

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OnilneCourseFunctions.Email;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient(); // Register HttpClient
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IEmailNotification, EmailNotification>();
+
     })
+     .ConfigureLogging(logging =>
+     {
+         logging.AddConsole();
+     })
     .Build();
 
 host.Run();
